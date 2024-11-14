702887a43109dfacd3b06c3891ea508efc234874
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-11-14 21:04:25 +0200

    global styles with styled components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
 import styled from 'styled-components';
 
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+
 const H1 = styled.h1`
     font-size: 30px;
     font-weight: 600;
     background-color: yellow;
-`;
-
-const Button = styled.button`
-    font-size: 1.4rem;
-    padding: 1.2rem 1.6rem;
-    font-weight: 500;
-    border: none;
-    border-radius: 7px;
-    background-color: purple;
-    color: white;
-    cursor: pointer;
-    margin: 20px;
-`;
-
-const Input = styled.input`
-    border: 1px solid #ddd;
-    border-radius: 5px;
-    padding: 0.8rem 1.2rem;
 `;
 
 const StyledApp = styled.div`
@@ -31,14 +17,22 @@ const StyledApp = styled.div`
 
 function App() {
     return (
-        /* StyledApp вместо div компонента App - согласно документации Styled Components */
-        <StyledApp>
-            <H1>Wild Oasis</H1>
-            <Button onClick={() => console.log('Clicked')}>Check in</Button>
-            <Button onClick={() => console.log('Clicked')}>Check out</Button>
+        <>
+            {/* подключаем глобальные стили,
+            GlobalStyles не может иметь никаких вложений (children) */}
+            <GlobalStyles />
+            {/* StyledApp вместо div компонента App - согласно документации
+            Styled Components  */}
+            <StyledApp>
+                <H1>Wild Oasis</H1>
+                <Button onClick={() => console.log('Clicked')}>Check in</Button>
+                <Button onClick={() => console.log('Clicked')}>
+                    Check out
+                </Button>
 
-            <Input type="number" placeholder="Number of guests" />
-        </StyledApp>
+                <Input type="number" placeholder="Number of guests" />
+            </StyledApp>
+        </>
     );
 }
 
