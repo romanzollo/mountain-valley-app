4c575928fe5de67aa451e659ee0c5e99c5e17350
Roman Zlagodukhin <zloyandmad@yandex.ru>
2025-01-13 15:19:06 +0200

    built the dashboard layout and dashboard filter

---
+++ src/features/dashboard/DashboardLayout.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const StyledDashboardLayout = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: auto 34rem auto;
+    gap: 2.4rem;
+`;
+
+function DashboardLayout() {
+    return (
+        <StyledDashboardLayout>
+            <div>Statistics</div>
+            <div>Today&apos;s activities</div> {/* Today's activities */}
+            <div>Chart stay durations</div>
+            <div>Chart sales</div>
+        </StyledDashboardLayout>
+    );
+}
+
+export default DashboardLayout;
