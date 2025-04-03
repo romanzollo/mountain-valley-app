c438f45f7bafaefc38148e8d8aac9f73a8790e67
Roman Zlagodukhin <zloyandmad@yandex.ru>
2025-04-03 22:21:29 +0200

    added Uploader

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 import Logo from './Logo';
 import MainNav from './MainNav';
-// import Uploader from '../data/Uploader';
+import Uploader from '../data/Uploader';
 
 const StyledSidebar = styled.aside`
     background-color: var(--color-grey-0);
@@ -22,7 +22,7 @@ function Sidebar() {
             <MainNav />
 
             {/* для тестирования */}
-            {/* <Uploader /> */}
+            <Uploader />
         </StyledSidebar>
     );
 }
